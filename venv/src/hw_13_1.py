8b98b3d059438a4a723669a852a1efc1dc2397b8
Lysenko Nikita <nike7cr77@ya.ru>
2024-03-17 20:44:05 +0300

    add_files

---
+++ venv/src/hw_13_1.py
@@ -0,0 +1,71 @@
+import json
+from venv.config import operations_path
+
+
+def main():
+    class Category:
+        """Product category of our store."""
+        labels: set
+        description: str
+        products: set
+
+        products = []
+        labels = set()
+
+        @classmethod
+        def uniq_products(cls):
+            return len(cls.products)
+
+        @classmethod
+        def count_labels(cls):
+            return len(cls.labels)
+
+        def __init__(self, label, description, products):
+            self.label = label
+            self.description = description
+            self.products = products
+
+            add_to_products = [Category.products.append(product) for product in self.products if
+                               product not in self.products]
+            Category.labels.add(self.label)
+
+        def __repr__(self):
+            return f"category class - {self.label}"
+
+    class Product:
+        """product that we have in our store."""
+        label: str
+        description: str
+        price: float
+        amount: int
+
+        def __init__(self, label, description, price, amount):
+            self.label = label
+            self.description = description
+            self.price = price
+            self.amount = amount
+
+        def __repr__(self):
+            return f"product class - {self.label}"
+
+    def make_products():
+        with open(operations_path, encoding='utf-8') as f:
+            content = f.read()
+            json_file = json.loads(content)
+        # test_1 = json_file[0]['products']
+        categories = []
+        products = []
+        for category in json_file:
+            categories.append(category['name'])
+            categories[-1] = Category(category['name'], category['description'], category['products'])
+            for product in category['products']:
+                products.append(product['name'])
+                products[-1] = Product(product['name'], product['description'], product['price'], product['quantity'])
+        categories = list(dict.fromkeys(categories))  # возвращает уникальные элементы списка, сохраняя порядок.
+        products = list(dict.fromkeys(products))
+
+        return (categories, products)
+
+
+if __name__ == '__main__':
+    main()
