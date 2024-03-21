839b9171df4d62c42fed78713a4b0c3e374bd728
Nike_S7 <nike7cr77@ya.ru>
2024-03-21 20:12:10 +0300

    added new methods and test - change price, check new price, add_products

--- src/prod_category.py
+++ src/prod_category.py
@@ -1,5 +1,5 @@
 import json
-from config import operations_path
+from config import operations_path # - Здесь находится файл с продуктами
 
 
 class Category:
@@ -10,20 +10,36 @@ class Category:
 
     products = []
     labels = set()
+
     cnt_products = len(products)
 
     count_uniq_labels = len(labels)
 
 
     def __init__(self, label, description, products):
+        """Инициализирует объект класса Category."""
         self.label = label
         self.description = description
-        self.products = products
+        self.__products = products
 
-        add_to_products = [Category.products.append(product) for product in self.products]
+        # add_to_products = [Category.products.append(product) for product in self.products]
         Category.labels.add(self.label)
         Category.count_uniq_labels = len(Category.labels)
-        Category.cnt_products = len(products)
+        Category.cnt_products = len(Category.products)
+
+    def pud_products(self):
+        """Добавляет продукты в атрибут класса - products."""
+        for product in self.__products:
+            Category.products.append(product)
+            Category.count_uniq_labels = len(Category.labels)
+            Category.cnt_products = len(Category.products)
+        return Category.products
+
+    @property
+    def all_products(self):
+        for product in Category.products:
+            print(f"Продукт - {product['name']}.\nЦена - {product['price']}.\nОстаток - {product['quantity']}")
+        return Category.products
 
     def __repr__(self):
         return f"category_class - {self.label}"
@@ -44,7 +60,38 @@ class Product:
     def __repr__(self):
         return f"product_class - {self.label}"
 
+    @classmethod
+    def build_product(cls, string: str):
+        """Создает объект класса Product из строки формата - 'название-описание-стоимость-остаток'"""
+        label, description, price, amount = string.split('-')
+        amount = int(amount)
+        price = float(price)
+        return cls(label, description, price, amount)
+
+    @property
+    def product_price(self):
+        return f"Название - {self.label}, Стоимость товара - {self.price}"
+
+    @product_price.setter
+    def product_price(self, new_price):
+        while new_price < 0 and new_price is not float:
+            print("You put incorrect price, please enter new price")
+            new_price = float(input("enter new price in float format: "))
+        if new_price < self.price:
+            answer = input("Are you sure that you want to reduce cost?(y/n):").strip().lower()
+            if answer == 'y':
+                self.price = new_price
+                return self.price
+            else:
+                return self.price
+        else:
+            self.price = new_price
+        return self.price
+
+
+
 def make_products(path):
+    """Создает объекты классов Category и Products из файла json."""
     with open(path, encoding='utf-8') as f:
         content = f.read()
         json_file = json.loads(content)
@@ -59,4 +106,7 @@ def make_products(path):
     categories = list(dict.fromkeys(categories))  # возвращает уникальные элементы списка, сохраняя порядок.
 
     return (categories, products)
+
+test_1 = make_products(operations_path)
+
 
--- tests/test_src.py
+++ tests/test_src.py
@@ -10,15 +10,32 @@ def products():
     prod_3 = Product('banana', 'huge banana', 145, 20)
     return prod_1, prod_2, prod_3
 
+
 @pytest.fixture
 def category(products):
     cat_1 = Category('Fruits', 'sweet fruits', products)
     return cat_1
 
+
 @pytest.fixture
 def make_prod():
     make_1 = make_products(operations_path)
     return make_1
+
+
+@pytest.fixture
+def pud_products(make_prod):
+    make_prod[0][0].pud_products()
+    make_prod[0][1].pud_products()
+    return Category.products
+
+
+@pytest.fixture
+def prod_price_setter(make_prod):
+    make_prod[1][0].product_price = 200_000
+    make_prod[1][1].product_price = 5
+    return make_prod[1][0].price, make_prod[1][1].price
+
 
 def test_products(products):
     assert products[0].label == 'apple'
@@ -26,13 +43,39 @@ def test_products(products):
     assert products[2].label == 'banana'
     assert products[2].amount == 20
 
+
 def test_category(category):
-    assert category.label =='Fruits'
+    assert category.label == 'Fruits'
     assert category.description == 'sweet fruits'
-    assert category.count_uniq_labels == 1
+    assert category.count_uniq_labels == 3
+
 
 def test_make_prod(make_prod):
     assert make_products(operations_path)[0][0].label == 'Смартфоны'
     assert make_products(operations_path)[0][1].label == 'Телевизоры'
     assert make_products(operations_path)[1][1].label == 'Iphone 15'
     assert make_products(operations_path)[0][1].count_uniq_labels == 3
+
+
+def test_prod_price(prod_price_setter):
+    assert prod_price_setter[0] == 200_000
+    assert prod_price_setter[1] == 5
+
+
+def test_pud_prod(pud_products):
+    assert pud_products == [{'description': '256GB, Серый цвет, 200MP камера',
+                             'name': 'Samsung Galaxy C23 Ultra',
+                             'price': 180000.0,
+                             'quantity': 5},
+                            {'description': '512GB, Gray space',
+                             'name': 'Iphone 15',
+                             'price': 210000.0,
+                             'quantity': 8},
+                            {'description': '1024GB, Синий',
+                             'name': 'Xiaomi Redmi Note 11',
+                             'price': 31000.0,
+                             'quantity': 14},
+                            {'description': 'Фоновая подсветка',
+                             'name': '55" QLED 4K',
+                             'price': 123000.0,
+                             'quantity': 7}]
